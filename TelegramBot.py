4ecc82bd5dc500995d76aae82b06e536a8afc5b7
Cleyfe <67307972+cleyfe@users.noreply.github.com>
2022-12-06 14:18:19 +0100

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -62,18 +62,10 @@ def telegrambot():
         message = "Nothing to report!"
         msg = telegram_bot_sendtext(message, TOKEN, bot_chatID)
         print(msg)
-        
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    print(content_type, chat_type, chat_id)
 
-    if content_type == 'text':
-        bot.sendMessage(chat_id, msg['text'])
-        
-        
-     schedule.every().day.at("12:05").do(telegrambot)
-schedule.every().day.at("00:05").do(telegrambot)
+ 
+#schedule.every().day.at("00:05").do(telegrambot)
 
-while True:
-    schedule.run_pending()
-    time.sleep(60)
+#while True:
+#    schedule.run_pending()
+#    time.sleep(60)
